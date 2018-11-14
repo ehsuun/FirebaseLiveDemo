b075e3f655c25c8293aa888a9ba07b0527ad4b80
Ehsan Sayyad <ehsan@mat.ucsb.edu>
2018-11-14 11:08:35 -0800

    just a working layout

---
+++ app/src/main/java/edu/ucsb/cs/cs184/ehsan/cs184firebaselive/MainActivity.java
@@ -0,0 +1,44 @@
+package edu.ucsb.cs.cs184.ehsan.cs184firebaselive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button login;
+    private Button logout;
+    private  Button read;
+    private  Button write;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.login).setOnClickListener(this);
+        findViewById(R.id.logout).setOnClickListener(this);
+        findViewById(R.id.read).setOnClickListener(this);
+        findViewById(R.id.write).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.logout:
+                Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.read:
+                Toast.makeText(this, "read", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.write:
+                Toast.makeText(this, "write", Toast.LENGTH_SHORT).show();
+            break;
+        }
+    }
+}
